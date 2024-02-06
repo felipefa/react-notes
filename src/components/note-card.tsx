b7350cda83e6afd7f74c78bc9462d1695f101ac7
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-06 18:15:18 +0100

    feat: add hover and focus-visible styles to note card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
       <span className="text-sm font-medium text-slate-300">2 days ago</span>
 
       <p className="text-sm leading-6 text-slate-400">
@@ -10,6 +10,6 @@ export function NoteCard() {
       </p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/50 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   );
 }
