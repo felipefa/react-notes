c284de7742ce4a4f62f0ee70eaa2bf39d04ab943
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-06 18:15:36 +0100

    fix: import logo from correct path

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-import logo from './assets/react-notes-logo.svg';
+import logo from '../assets/react-notes-logo.svg';
 
 export function Logo() {
   return <img alt="React Notes Logo" src={logo} />;
