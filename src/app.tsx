1471eee93e56a8c6010f459fa5431a63a8bff7eb
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-06 16:30:41 +0100

    feat: add app logo

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
+import logo from './assets/react-notes-logo.svg';
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <img src={logo} />
+      <input placeholder="Search notes..." type="text" />
+    </div>
+  );
 }
