e4e4ffea9a2f9d579c2e20b4271f59211d9ed3db
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-06 14:07:54 +0100

    chore: add Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
