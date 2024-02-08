615b2bbc1b41bbe3a4b5c874b0ac573b8eec051f
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-08 15:55:31 +0100

    feat: update Logo component to include a link to the GitHub repository

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,14 @@
 import logo from '../assets/react-notes-logo.svg';
 
 export function Logo() {
-  return <img alt="React Notes Logo" src={logo} />;
+  return (
+    <a
+      className="block w-fit"
+      href="https://github.com/felipefa/react-notes"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img alt="React Notes Logo" src={logo} />
+    </a>
+  );
 }
